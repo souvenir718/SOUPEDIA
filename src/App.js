fa1648598b22e8d7caccf31a3063a1ba88f21471
souvenir718 <mongkey0@naver.com>
2021-01-27 09:24:19 +0900

    prop-types 사용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const foodILike = [
     {
@@ -11,7 +12,7 @@ const foodILike = [
         id: 2,
         name: 'Samgyeopsal',
         image: 'https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg',
-        rating: 5.5,
+        rating: 3.5,
     },
     {
         id: 3,
@@ -34,20 +35,27 @@ const foodILike = [
     },
 ];
 
-function Food({ name, picture }) {
+function Food({ name, picture, rating }) {
     return (
         <div>
             <h2>I like {name}</h2>
+            <h4>{rating} / 5.0</h4>
             <img src={picture} alt={name} />
         </div>
     );
 }
 
+Food.propTypes = {
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+};
+
 function App() {
     return (
         <div>
             {foodILike.map((dish) => (
-                <Food key={dish.id} name={dish.name} picture={dish.image} />
+                <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
             ))}
         </div>
     );
