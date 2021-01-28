9ff747e0269a4bc0d050d9d2c13b4023f94c800f
souvenir718 <mongkey0@naver.com>
2021-01-28 09:02:46 +0900

    deploy ghpage

--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import './Movie.css';
 
-function Movie({ id, year, title, summary, poster }) {
-    return <h4>{title}</h4>;
+function Movie({ year, title, summary, poster, genres }) {
+    return (
+        <div className="movie">
+            <img src={poster} alt={title} title={title} />
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <p className="movie__summary">{summary.length > 140 ? summary.slice(0, 140) + '...' : summary}</p>
+                <ul className="geners">
+                    {genres.map((genere, idx) => (
+                        <li key={idx} className="generes__genre">
+                            {genere}
+                        </li>
+                    ))}
+                </ul>{' '}
+            </div>
+        </div>
+    );
 }
 
 Movie.propTypes = {
@@ -11,6 +28,7 @@ Movie.propTypes = {
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
