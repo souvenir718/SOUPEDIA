7ea5baa1c8a0bf76f451e21e70d9ffe612ceea2e
souvenir718 <mongkey0@naver.com>
2021-01-28 09:40:54 +0900

    how to Link tag

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HashRouter, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
 import About from './routes/About';
 import Home from './routes/Home';
 /*
@@ -100,6 +101,7 @@ class App extends React.Component {
 function App() {
     return (
         <HashRouter>
+            <Navigation />
             <Route path="/" exact={true} component={Home} />
             <Route path="/about" component={About} />
         </HashRouter>
