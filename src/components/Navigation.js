e41fce4f0a07865d45b4ab24276d0c152d3d73e2
souvenir718 <mongkey0@naver.com>
2021-02-02 09:30:04 +0900

    헤더 수정중..

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './Navigation.css';
 function Navigation() {
     return (
-        <div>
+        <div className="header">
+            <span className="header__souvenir">souvenir</span>
+            <span className="header__pedia">PEDIA</span>
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
         </div>
