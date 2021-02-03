bdae1487c280ec84245ded720c4c08017426a914
souvenir718 <mongkey0@naver.com>
2021-02-03 09:06:40 +0900

    헤더 완성

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,10 +4,16 @@ import './Navigation.css';
 function Navigation() {
     return (
         <div className="header">
-            <span className="header__souvenir">souvenir</span>
-            <span className="header__pedia">PEDIA</span>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
+            <Link to="/">
+                <img src={`${process.env.PUBLIC_URL}/logo2.png`} alt="logo" title="logo" className="header__logo" />
+            </Link>
+            <a href="https://souvenir718.github.io/">
+                <img src={`${process.env.PUBLIC_URL}/portfolio.png`} alt="logo" title="portfolio page" className="header__portfolio" />
+            </a>
+            <a href="https://github.com/souvenir718">
+                <img src={`${process.env.PUBLIC_URL}/github.jpg`} alt="logo" title="souvenir github" className="header__github" />
+            </a>
+            {/* <Link to="/about">About</Link> */}
         </div>
     );
 }
